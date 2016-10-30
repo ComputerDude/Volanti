ed04821fcc7329175fbc0fc9cbf70323b79afdf6
Justin Brubaker <justbru00@gmail.com>
2016-10-30 18:51:42 -0400

    Fixed some errors and shortened code.

--- ConsolePlugin/src/com/computerdude/volanti/CustomEnchants.java
+++ ConsolePlugin/src/com/computerdude/volanti/CustomEnchants.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType.SlotType;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -15,58 +16,88 @@ public class CustomEnchants implements Listener {
 
 	@EventHandler
 	public void onInventoryClick(InventoryClickEvent e) {
+		
+		/**
+		 * @author Justin Brubaker
+		 */
+		if (e.getInventory() == null) return;
+		
+		
 		Player player = (Player) e.getWhoClicked();
-		if (e.getSlot() == 100
-				&& player.getInventory().getBoots().getItemMeta().getLore().contains(Main.color("&7&lCheetah I"))) {
+		ArrayList<String> itemLore = player.getInventory().getBoots().getItemMeta().getLore();
+		
+		SlotType testing = SlotType.ARMOR;
+		
+		if (e.getSlotType() == testing && itemLore.contains(Main.color("&7&lCheetah I"))) {
+			
 			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 99999, 1));
-		} else if (e.getSlot() == 100
-				&& player.getInventory().getBoots().getItemMeta().getLore().contains(Main.color("&7&lCheetah II"))) {
+			
+		} else if (e.getSlotType() == testing && itemLore.contains(Main.color("&7&lCheetah II"))) {
+			
 			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 99999, 2));
-		} else if (e.getSlot() == 100
-				&& player.getInventory().getBoots().getItemMeta().getLore().contains(Main.color("&7&lCheetah III"))) {
+			
+		} else if (e.getSlotType() == testing && itemLore.contains(Main.color("&7&lCheetah III"))) {
+			
 			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 99999, 3));
-		} else if (!player.getInventory().getBoots().getItemMeta().getLore().contains(Main.color("&7&lCheetah III"))
-				|| !player.getInventory().getBoots().getItemMeta().getLore().contains(Main.color("&7&lCheetah II"))
-				|| !player.getInventory().getBoots().getItemMeta().getLore().contains(Main.color("&7&lCheetah I"))) {
+			
+		} else if (!itemLore.contains(Main.color("&7&lCheetah III")) || !itemLore.contains(Main.color("&7&lCheetah II")) || !itemLore.contains(Main.color("&7&lCheetah I"))) {
+			
 			player.removePotionEffect(PotionEffectType.SPEED);
 		}
 	}
 
 	@EventHandler
 	public void onEntityAttack(EntityDamageByEntityEvent e) {
+		
+		/**
+		 * @author Justin Brubaker
+		 */
+		if (e.getEntity().getType() != EntityType.PLAYER) return; 		
+		
 		if (e.getDamager().getType().equals(EntityType.PLAYER) && e.getEntity().getType().equals(EntityType.PLAYER)) {
+			
 			Player player = (Player) e.getDamager();
 			Player attacked = (Player) e.getEntity();
-			if (player.isSneaking() == true && player.getInventory().getItemInMainHand().getItemMeta().getLore()
-					.contains(Main.color("&7Execute I"))) {
+			ArrayList<String> itemLore = player.getInventory().getBoots().getItemMeta().getLore();
+			
+			boolean sneaking = player.isSneaking();
+			
+			if (sneaking && itemLore.contains(Main.color("&7Execute I"))) {
+				
 				player.sendMessage("True");
-				double damage = e.getFinalDamage() + 1;
+				double damage = e.getFinalDamage() + 1;				
 				e.setDamage(damage);
-			} else if (player.isSneaking() == true && player.getInventory().getItemInMainHand().getItemMeta().getLore()
-					.contains(Main.color("&7Execute II"))) {
+				
+			} else if (sneaking && itemLore.contains(Main.color("&7Execute II"))) {
+				
 				player.sendMessage("True");
 				double damage = e.getFinalDamage() + 2;
 				e.setDamage(damage);
-			} else if (player.isSneaking() == true && player.getInventory().getItemInMainHand().getItemMeta().getLore()
-					.contains(Main.color("&7Execute III"))) {
+				
+			} else if (sneaking == true && itemLore.contains(Main.color("&7Execute III"))) {
+				
 				player.sendMessage("True");
 				double damage = e.getFinalDamage() + 3;
 				e.setDamage(damage);
-			} else if (player.isSneaking() == true && player.getInventory().getItemInMainHand().getItemMeta().getLore()
-					.contains(Main.color("&7Execute IV"))) {
+				
+			} else if (sneaking == true && itemLore.contains(Main.color("&7Execute IV"))) {
+				
 				player.sendMessage("True");
 				double damage = e.getFinalDamage() + 4;
 				e.setDamage(damage);
-			} else if (player.isSneaking() == true && player.getInventory().getItemInMainHand().getItemMeta().getLore()
-					.contains(Main.color("&7Execute V"))) {
+				
+			} else if (sneaking == true && itemLore.contains(Main.color("&7Execute V"))) {
+				
 				player.sendMessage("True");
 				double damage = e.getFinalDamage() + 5;
 				e.setDamage(damage);
-			} else if (player.isSneaking() == true && player.getInventory().getItemInMainHand().getItemMeta().getLore()
-					.contains(Main.color("&7Execute VI"))) {
+				
+			} else if (sneaking == true && itemLore.contains(Main.color("&7Execute VI"))) {
+				
 				player.sendMessage("True");
 				double damage = e.getFinalDamage() + 6;
 				e.setDamage(damage);
+				
 			} else {
 				player.sendMessage("False");
 			}
